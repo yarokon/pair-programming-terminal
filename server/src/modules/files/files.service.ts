b6dd4d77140262f21cdeb35d63765f6e8619ff5d
Anton <krava34007@gmail.com>
2026-06-17 12:37:05 +0300

    chore: implement file existence check and enhance createFileTree method

--- server/src/modules/files/files.service.ts
+++ server/src/modules/files/files.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import fs from 'node:fs';
 import path from 'node:path';
+import { stat } from 'node:fs/promises';
 import { simpleGit } from 'simple-git';
+
+// const TYPE = {
+//   FILE: 'file',
+//   DIRECRORY: 'directory',
+// } as const;
+
+// TYPE[key]
+
+// type FileNodeType = keyof (typeof TYPE)[keyof typeof TYPE];
 
 // TODO: Change to discriminated union type
 export type FileNode = {
@@ -11,6 +21,17 @@ export type FileNode = {
   children?: FileNode[];
 };
 
+// type is file ? abc : abd
+
+// type FileNodeOnly = {
+//   name: string;
+//   type: 'file' ;
+// };
+
+// const isFile = (file: FileNode) file is FileNodeOnly => {
+//  entry.isDirectory()
+// }
+
 @Injectable()
 export class FilesService {
   /**
@@ -18,8 +39,10 @@ export class FilesService {
    * [] Use cloneRepo
    * [] Call createFileTree
    */
+
   public async getFiles(owner: string, repoName: string): Promise<FileNode[]> {
-    return [];
+    const currentDir = await this.cloneRepo(owner, repoName);
+    return this.createFileTree(currentDir);
   }
 
   /**
@@ -37,16 +60,43 @@ export class FilesService {
   //   return '';
   // }
 
+  private async fileExists(path: string) {
+    try {
+      await stat(path);
+      return true;
+    } catch (error) {
+      throw error; // Rethrow other unexpected errors (e.g., permission issues)
+    }
+  }
+
   /**
    * TODO list:
    * [] Clone repo
    * [] Handle error if clone repo second time fails
    */
-  private async cloneRepo(owner: string, repoName: string): Promise<void> {
+  private async cloneRepo(owner: string, repoName: string): Promise<string> {
     const repoUrl = `https://github.com/${owner}/${repoName}.git`;
-    const repoDir = path.join('repositories', owner, repoName); // TODO: Use absolute path
+
+    const absolutePath = path.resolve(`repositories/${owner}/${repoName}`);
+
+    const repoAlreadyExists = await this.fileExists(absolutePath);
+
+    if (repoAlreadyExists) {
+      // throw new Error('This repo is already exist');
+      console.log('This repo is already exist');
+      return absolutePath;
+    }
+    const repoDir = path.join(absolutePath, owner, repoName); // TODO: Use absolute path
 
     const git = simpleGit(); // https://github.com/steveukx/git-js
+
+    try {
+      await git.clone(repoUrl, repoDir);
+    } catch (error) {
+      console.error(error);
+    }
+
+    return absolutePath;
   }
 
   /**
@@ -58,9 +108,34 @@ export class FilesService {
    */
   private async createFileTree(currentDir: string): Promise<FileNode[]> {
     const entries = await fs.promises.readdir(currentDir, {
-      withFileTypes: true, // Use entry.isDirectory() and entry.name
+      withFileTypes: true,
     });
 
-    return [];
+    const folders = entries.filter((entry) => entry.isDirectory());
+
+    const files = entries.filter((entry) => !entry.isDirectory());
+
+    const formatedFiles = files.map((file) => ({
+      name: file.name,
+      type: 'file' as const,
+    }));
+
+    let result: FileNode[] = [...formatedFiles];
+
+    for (const folder of folders) {
+      const fullPath = path.join(currentDir, folder.name);
+
+      const subFolders = await this.createFileTree(fullPath);
+
+      const formatedFolder = {
+        name: folder.name as string,
+        type: 'directory' as const,
+        children: subFolders,
+      };
+
+      result.push(formatedFolder);
+    }
+
+    return result;
   }
 }
