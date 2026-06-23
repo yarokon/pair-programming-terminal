b9e1f3479230cf68e5f5878fa8e62afd8a379427
Tetiana Melnychuk <tanya.melnychukk@gmail.com>
2026-06-23 11:50:05 +0300

    pair programming task

--- server/src/modules/files/files.service.ts
+++ server/src/modules/files/files.service.ts
@@ -6,7 +6,7 @@ import { simpleGit } from 'simple-git';
 // TODO: Change to discriminated union type
 export type FileNode = {
   name: string;
-  // path: string;
+  path: string;
   type: 'file' | 'directory';
   children?: FileNode[];
 };
@@ -19,7 +19,14 @@ export class FilesService {
    * [] Call createFileTree
    */
   public async getFiles(owner: string, repoName: string): Promise<FileNode[]> {
-    return [];
+    const repoDir = path.join(__dirname, '../../../', 'repositories', owner, repoName);
+    
+    await this.cloneRepo(repoDir, owner, repoName);
+
+    const fileTree = await this.createFileTree(repoDir);
+
+    console.log(fileTree);
+    return fileTree;
   }
 
   /**
@@ -42,11 +49,17 @@ export class FilesService {
    * [] Clone repo
    * [] Handle error if clone repo second time fails
    */
-  private async cloneRepo(owner: string, repoName: string): Promise<void> {
+  private async cloneRepo(repoDir: string, owner: string, repoName: string): Promise<void> {
     const repoUrl = `https://github.com/${owner}/${repoName}.git`;
-    const repoDir = path.join('repositories', owner, repoName); // TODO: Use absolute path
-
+  
     const git = simpleGit(); // https://github.com/steveukx/git-js
+    
+    try {
+      await git.clone(repoUrl, repoDir);
+    } catch (error) {
+      // TODO: handle correct existing directory error
+      await git.cwd(repoDir).pull();
+    }
   }
 
   /**
@@ -56,11 +69,36 @@ export class FilesService {
    * [] return relative path
    * [] sort (directories first) // entryA.name.localeCompare(entryB.name, 'en-US', { numeric: true, sensitivity: 'base' });
    */
-  private async createFileTree(currentDir: string): Promise<FileNode[]> {
+  private async createFileTree(currentDir: string, parentPath: string = ''): Promise<FileNode[]> {
     const entries = await fs.promises.readdir(currentDir, {
       withFileTypes: true, // Use entry.isDirectory() and entry.name
     });
 
-    return [];
+    console.log(currentDir);
+
+    const fileTree: FileNode[] = [];
+
+    for (const entry of entries) {
+      if (entry.name === '.git') {
+        continue;
+      }
+
+      const entityPath = path.join(parentPath, entry.name); 
+
+      const fileTreeEntry: FileNode = { 
+        name: entry.name,
+        path: entityPath,
+        type: entry.isDirectory() ? 'directory' : 'file',
+      }
+
+      
+      if (entry.isDirectory()) {
+        fileTreeEntry.children = await this.createFileTree(`${currentDir}/${entry.name}`, entityPath);
+      }
+      
+      fileTree.push(fileTreeEntry);
+    }
+    
+    return fileTree;
   }
 }
